7a415e7fddc3806f0454ce7f85699e55fc2a92a4
Dean Perry <dean@deanpcmad.com>
2024-05-20 19:43:02 +0100

    tweaks

--- ubuntu.sh
+++ ubuntu.sh
@@ -24,8 +24,6 @@ then
   rm -rf google-chrome-stable_current_amd64.deb
 fi
 
-
-
 # Remove Firefox Snap and install from Mozilla
 sudo apt remove -y firefox
 sudo snap remove firefox
@@ -33,13 +31,10 @@ sudo install -d -m 0755 /etc/apt/keyrings
 wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
 gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
 echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-echo '
-Package: *
+echo "Package: *
 Pin: origin packages.mozilla.org
-Pin-Priority: 1000
-' | sudo tee /etc/apt/preferences.d/mozilla
+Pin-Priority: 1000" | sudo tee /etc/apt/preferences.d/mozilla
 sudo apt-get update && sudo apt-get install firefox
-
 
 
 # Atuin
